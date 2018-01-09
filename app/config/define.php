0c04a28d331152050239eca81ba915d2c2c2f4b3
Minh <hungquocminh1989@gmail.com>
2018-01-09 11:46:57 +0700

    uodate

--- app/config/define.php
+++ app/config/define.php
@@ -3,4 +3,3 @@ define('SYSTEM_PASSCODE', '15031989');
 define('SYSTEM_ROOT_DIR', SYSTEM_PUBLIC_DIR);
 define('SYSTEM_PUBLIC_UPLOAD', SYSTEM_ROOT_DIR.'/public/upload');
 define('SYSTEM_CURRENCY', ' đ');
-
--- app/controllers/Initialize.php
+++ app/controllers/Initialize.php
@@ -1,49 +1,40 @@
 <?php
-namespace app\controllers;
+session_start();
 
-use Flight;
+// Optional Parameters = /(index.php) means you can either use / OR /index.php 
+// Named Parameters = @name will pass $name into your callback function
+// Wildcard = *
 
-class Initialize {
-    
-    public function __construct()
-    {
-        // Optional Parameters = /(index.php) means you can either use / OR /index.php 
-        // Named Parameters = @name will pass $name into your callback function
-        // Wildcard = *
-        
-        Flight::set('flight.views.path', 'app/views/');
-        
-        // Initialize Controller
-        $controller = new CommonController();
-        Flight::route('/(index)', array($controller, 'index'));
-        Flight::route('/detail/@id/@product_link', array($controller, 'detail'));
-        Flight::route('/main', array($controller, 'main'));
-        Flight::route('/admin', array($controller, 'adminlogin'));
-        Flight::route('/admin/login', array($controller, 'login'));
-        
-        //Category
-        Flight::route('/main/category/add', array($controller, 'addcategory'));
-        Flight::route('/main/category/update', array($controller, 'updatecategory'));
-        Flight::route('/main/category/delete', array($controller, 'deletecategory'));
-        
-        //Product
-        Flight::route('/main/product/add', array($controller, 'addproduct'));
-        Flight::route('/main/product/update', array($controller, 'updateproduct'));
-        Flight::route('/main/product/delete', array($controller, 'deleteproduct'));
-        
-        // Membership Controller
-        /*$membership = new MembershipController();
-        Flight::route('GET /login', array($membership, 'login'));
-        Flight::route('POST /login', array($membership, 'loginAttempt'));
-        Flight::route('/logout', array($membership, 'logout'));
-        Flight::route('/profile/@name', array($membership, 'profile'));
-        Flight::route('GET /profile/@name/edit', array($membership, 'profileEdit'));
-        Flight::route('POST /profile/@name/edit', array($membership, 'profileEditAttempt'));
-        Flight::route('GET /sign-up', array($membership, 'register'));
-        Flight::route('POST /sign-up', array($membership, 'registerAttempt'));*/
-        
-        // catch everything
-        Flight::route('/*', array($controller, 'index'));
-    }
-    
-}
+Flight::set('flight.views.path', 'app/views/');
+
+// Initialize Controller
+$controller = new app\controllers\CommonController();
+Flight::route('/(index)', array($controller, 'index'));
+Flight::route('/detail/@id/@product_link', array($controller, 'detail'));
+Flight::route('/main', array($controller, 'main'));
+Flight::route('/admin', array($controller, 'adminlogin'));
+Flight::route('/admin/login', array($controller, 'login'));
+
+//Category
+Flight::route('/main/category/add', array($controller, 'addcategory'));
+Flight::route('/main/category/update', array($controller, 'updatecategory'));
+Flight::route('/main/category/delete', array($controller, 'deletecategory'));
+
+//Product
+Flight::route('/main/product/add', array($controller, 'addproduct'));
+Flight::route('/main/product/update', array($controller, 'updateproduct'));
+Flight::route('/main/product/delete', array($controller, 'deleteproduct'));
+
+// Membership Controller
+/*$membership = new MembershipController();
+Flight::route('GET /login', array($membership, 'login'));
+Flight::route('POST /login', array($membership, 'loginAttempt'));
+Flight::route('/logout', array($membership, 'logout'));
+Flight::route('/profile/@name', array($membership, 'profile'));
+Flight::route('GET /profile/@name/edit', array($membership, 'profileEdit'));
+Flight::route('POST /profile/@name/edit', array($membership, 'profileEditAttempt'));
+Flight::route('GET /sign-up', array($membership, 'register'));
+Flight::route('POST /sign-up', array($membership, 'registerAttempt'));*/
+
+// catch everything
+Flight::route('/*', array($controller, 'index'));

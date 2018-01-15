07a5a1676fa4f1a229a0b1dc0493b68c83ab9a4b
Minh <hungquocminh1989@gmail.com>
2018-01-15 21:26:49 +0700

    update

--- app/config/define.php
+++ app/config/define.php
@@ -8,3 +8,4 @@ define('SYSTEM_PASSCODE', '15031989');
 define('SYSTEM_ROOT_DIR', SYSTEM_PUBLIC_DIR);
 define('SYSTEM_PUBLIC_UPLOAD', SYSTEM_ROOT_DIR.'/public/upload');
 define('SYSTEM_CURRENCY', ' đ');
+define('SYSTEM_PHONE', '0902660153');
